aa43ae44bf69a9e746ee3cb0b535eb22dee40ac0
young <tlszn121@sookmyung.ac.kr>
2021-12-03 17:41:47 +0900

    fix:dbName

--- config/config.js
+++ config/config.js
@@ -10,14 +10,14 @@ module.exports={
   "test": {
     "username": "root",
     "password": process.env.SEQUELIZE_PASSWORD,
-    "database": "lithm_test",
+    "database": "lithm",
     "host": "127.0.0.1",
     "dialect": "mysql"
   },
   "production": {
     "username": "root",
     "password": process.env.SEQUELIZE_PASSWORD,
-    "database": "lithm_production",
+    "database": "lithm",
     "host": "127.0.0.1",
     "dialect": "mysql"
   }
